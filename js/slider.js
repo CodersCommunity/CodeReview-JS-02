2b53b5409e334df757a0e13993900fb05483502a
magic96 <qrdupel1996@gmail.com>
2016-08-06 02:08:46 +0200

    Rewrote code to ES6.

---
+++ js/slider.js
@@ -0,0 +1,43 @@
+{
+  'use strict';
+
+  const sliderElement = document.getElementById('slider');
+  const navElement = document.getElementById('nav');
+  const slideDir = './slajdy';
+  const slideTime = 5000;
+  const slideSources = ['slajd1.png', 'slajd2.png', 'slajd3.png', 'slajd4.png', 'slajd5.png'];
+  const slides = [];
+  let currentSlide;
+  let sliderTimeout;
+
+  const setSlide = slideIndex => {
+    currentSlide = slideIndex >= slideSources.length ? 0 : slideIndex;
+    slides.forEach(slide => slide.classList.remove('visible'));
+    slides[currentSlide].classList.add('visible');
+    clearTimeout(sliderTimeout);
+    sliderTimeout = setTimeout(() => setSlide(++currentSlide), slideTime);
+  };
+
+  const addSlide = slideSource => {
+    const slide = document.createElement('img');
+    const link = document.createElement('a');
+    const slideIndex = slides.push(slide) - 1;
+
+    slide.src = `${slideDir}/${slideSource}`;
+    slide.classList.add('slide');
+
+    link.href = `#${slideIndex}`;
+    link.addEventListener('click', () => setSlide(slideIndex), false)
+    link.classList.add('nav__link');
+
+    sliderElement.appendChild(slide);
+    navElement.appendChild(link);
+  };
+
+  slideSources.forEach(source => addSlide(source));
+
+  setTimeout(() => {
+    const randomSlideIndex = Math.floor(Math.random() * slides.length);
+    setSlide(randomSlideIndex);
+  }, 500);
+}
